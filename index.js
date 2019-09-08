2b06a0b2516e0de63ac4d72c972dfc65cc08da13
Gözde <gozde.baskus@yandex.com>
2019-09-08 19:46:06 +0300

    - Multiple project support

--- index.js
+++ index.js
@@ -12,60 +12,69 @@ try {config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'))} catch (e) {}
 const slack = new Slack();
 slack.setWebhook(config.slack.webhook);
 
-const handler = createHandler([{path: config.bitbucket.handler}]);
+const handlers = [];
+config.bitbucket.projects.forEach((project)=>{
+    const handler = createHandler([{path: project.handler}]);
+    handlers.push(handler);
 
-http.createServer(function (req, res) {
-    handler(req, res, function (err) {
-        res.statusCode = 404;
-        res.end()
-    })
-}).listen(config.server.port);
+    handler.on('error', function (err) {
+        console.error('Error:', err.message);
+    });
 
-handler.on('error', function (err) {
-    console.error('Error:', err.message)
-});
+    handler.on('push', function (event) {
+        const newEvent = event.payload.push.changes[0].new;
+        const isForcePush = event.payload.push.changes[0].forced;
+        const name = newEvent.name;
+        const summary = newEvent.target.summary.raw;
+        const author = newEvent.target.author.raw;
 
-handler.on('push', function (event) {
-    const newEvent = event.payload.push.changes[0].new;
-    const isForcePush = event.payload.push.changes[0].forced;
-    const name = newEvent.name;
-    const summary = newEvent.target.summary.raw;
-    const author = newEvent.target.author.raw;
+        console.log('Received a push event:', name, newEvent, summary, author);
 
-    console.log('Received a push event:', name, newEvent, summary, author);
+        const {branches} = project;
+        if (Object.keys(branches).indexOf((name)) !== -1) {
+            const branchConfig = branches[name];
 
-    const {branches} = config.bitbucket;
-    if (Object.keys(branches).indexOf((name)) !== -1) {
-        const branchConfig = branches[name];
+            if (branchConfig.deployment) {
+                if (branchConfig.gitreset) {
+                    resetProject(branchConfig.deployment, branchConfig);
+                } else if (branchConfig.npminstall) {
+                    pullProject(branchConfig.deployment, branchConfig);
+                }else if (branchConfig.npmBuild) {
+                    npmBuild(branchConfig.deployment, branchConfig);
+                }
+            }
 
-        if (branchConfig.deployment) {
-            if (branchConfig.gitreset) {
-                resetProject(branchConfig.deployment, branchConfig);
-            } else if (branchConfig.npminstall) {
-                pullProject(branchConfig.deployment, branchConfig);
+            if (branchConfig.notification) {
+                slack.webhook({
+                    channel: config.slack.channel,
+                    username: config.slack.username,
+                    "text": "*" + author.split(" ")[0] + "* changed `" + name + "` branch. " + (isForcePush ? "`*FORCE PUSH*`" : ""),
+                    "attachments": [
+                        {
+                            "title": summary,
+                            "color": "#3AA3E3",
+                            "attachment_type": "default",
+                        }
+                    ]
+
+                }, function (err, response) {
+                    console.log(response);
+                });
             }
         }
+    });
 
-
-        if (branchConfig.notification) {
-            slack.webhook({
-                channel: config.slack.channel,
-                username: config.slack.username,
-                "text": "*" + author.split(" ")[0] + "* changed `" + name + "` branch. " + (isForcePush ? "`*FORCE PUSH*`" : ""),
-                "attachments": [
-                    {
-                        "title": summary,
-                        "color": "#3AA3E3",
-                        "attachment_type": "default",
-                    }
-                ]
-
-            }, function (err, response) {
-                console.log(response);
-            });
-        }
-    }
 });
+
+
+http.createServer(function (req, res) {
+    handlers.forEach((handler)=>{
+        handler(req, res, function (err) {
+            res.statusCode = 404;
+            res.end()
+        })
+    });
+}).listen(config.server.port);
 
 console.info('Webhook started.');
 if(config.slack.welcome) {
@@ -105,6 +114,8 @@ function pullProject(projectPath, branchConfig) {
         console.log(`git pull exited with code ${code}`);
         if (branchConfig.npminstall) {
             npmInstall(projectPath, branchConfig);
+        } else if (branchConfig.npmBuild) {
+            npmBuild(branchConfig.deployment, branchConfig);
         }
     });
 
@@ -126,8 +137,23 @@ function npmInstall(projectPath, branchConfig) {
         console.log(`npm install exited with code ${code}`);
         restartApp(projectPath, branchConfig);
     });
+}
 
+function npmBuild(projectPath, branchConfig) {
 
+    let npminstall = spawn('npm', ['run','build'], {cwd: projectPath});
+
+    npminstall.stdout.on('data', (data) => {
+        console.log(`npm install stdout: ${data}`);
+    });
+
+    npminstall.stderr.on('data', (data) => {
+        console.log(`npm install stderr: ${data}`);
+    });
+
+    npminstall.on('close', (code) => {
+        console.log(`npm install exited with code ${code}`);
+    });
 }
 
 function restartApp(projectPath, branchConfig) {
@@ -145,6 +171,4 @@ function restartApp(projectPath, branchConfig) {
     npminstall.on('close', (code) => {
         console.log(`npm install exited with code ${code}`);
     });
-
-
 }
